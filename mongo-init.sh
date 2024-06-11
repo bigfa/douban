aca4ea6c94b5dc34fb552667bcfff095374f0089
bigfa <jigoulee@gmail.com>
2024-06-11 17:28:58 +0800

    chore

--- mongo-init.sh
+++
@@ -1,1 +0,0 @@
-mongo --eval "db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD'); db = db.getSiblingDB('$DB_NAME'); db.createUser({ user: '$MONGO_USER', pwd: '$MONGO_PASSWORD', roles: [{ role: 'readWrite', db: '$DB_NAME' }] });"
